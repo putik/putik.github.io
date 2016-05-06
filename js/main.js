47be949510eb959f73b5141af5828174550c43cb
Andre Christoga <andrechristoga@gmail.com>
2016-05-06 10:44:07 +0700

    Initial responsive script

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function (){
+    // Get window size
+    var width = $(window).width(),
+        height = $(window).height(),
+    // Minimized the width
+        costumizedWidth = width - 32,
+        minimizedHeight = height -  300,
+
+    if(navigator.userAgent.toLowerCase().indexOf("android") > -1) {
+      
+    }
+    else if (navigator.userAgent.toLowerCase().indexOf("iphone") > -1 ){
+
+    }
+});
