5f0e3628e3957da461fc21d06eae28f94b1d03e4
Andre Christoga <andrechristoga@gmail.com>
2016-05-06 10:47:09 +0700

    Responsive image

--- js/responsive.js
+++ js/responsive.js
@@ -5,11 +5,13 @@ $(document).ready(function (){
     // Minimized the width
         costumizedWidth = width - 32,
         minimizedHeight = height -  300,
+    // Get image by class
+        image = $('.pentatonix');
 
     if(navigator.userAgent.toLowerCase().indexOf("android") > -1) {
-
+      image.css('width', costumizedWidth)
     }
     else if (navigator.userAgent.toLowerCase().indexOf("iphone") > -1 ){
-
+      image.css('width', costumizedWidth)
     }
 });
